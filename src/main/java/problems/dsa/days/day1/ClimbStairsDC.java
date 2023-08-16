f06fe685921137829f46791907184f1daed6cd4d
razzaksr <razzaksr@gmail.com>
2023-08-16 14:10:23 +0530

    Day1 DP added

--- src/main/java/problems/dsa/days/day1/ClimbStairsDC.java
+++ src/main/java/problems/dsa/days/day1/ClimbStairsDC.java
@@ -19,6 +19,7 @@ public class ClimbStairsDC {
         dp[1] = 1;dp[2] = 2;
         for (int i = 3; i <= n; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
+            System.out.println(dp[i]);
         }
         return dp[n];
     }
